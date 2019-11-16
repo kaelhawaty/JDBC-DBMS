c38f92aa872144f26051050608b0c2e40cc14deb
HazemShawky <hazemahmed575@gmail.com>
2019-11-16 20:32:20 +0200

    initial commit

---
+++ src/eg/edu/alexu/csd/oop/db/cs2/DatabaseManager.java
@@ -0,0 +1,25 @@
+package eg.edu.alexu.csd.oop.db.cs2;
+
+import java.sql.SQLException;
+
+public class DatabaseManager implements Database{
+    @Override
+    public String createDatabase(String databaseName, boolean dropIfExists) {
+        return null;
+    }
+
+    @Override
+    public boolean executeStructureQuery(String query) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Object[][] executeQuery(String query) throws SQLException {
+        return new Object[0][];
+    }
+
+    @Override
+    public int executeUpdateQuery(String query) throws SQLException {
+        return 0;
+    }
+}
